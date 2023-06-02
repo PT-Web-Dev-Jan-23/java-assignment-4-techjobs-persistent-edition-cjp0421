814c3cc2b49c2c62190ef5dc33947b088a66cce6
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-06-02 08:46:44 -0500

    All task 2 tests pass 27/27; 30/44 total tests pass

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -9,10 +11,37 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Optional;
 
+
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("employers",employerRepository.findAll());
+        return "employers/index";
+    }
+
+    @GetMapping("view")
+    public String index(@RequestParam(required=false) Integer employerId, Model model){
+        if(employerId == null){
+            model.addAttribute("title", "All Employers");
+            model.addAttribute("employers", employerRepository.findAll());
+        } else {
+            Optional<Employer> result = employerRepository.findById(employerId);
+            if(result.isEmpty()){
+                model.addAttribute("title","Invalid Employer Id: " + employerId);
+            } else {
+                Employer employer = result.get();
+                model.addAttribute("title", "Employer with ID" + employerId);
+                model.addAttribute("employers",employer.getName());
+            }
+        }
+        return "employers/view";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -24,8 +53,13 @@ public class EmployerController {
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
 
-        if (errors.hasErrors()) {
-            return "employers/add";
+        if(!errors.hasErrors()) {
+
+            if (!employerRepository.toString().contains(newEmployer.getName())) {
+               Employer employer = employerRepository.save(newEmployer);
+                model.addAttribute("employer", employer);
+            }
+            return "redirect:";
         }
 
         return "redirect:";
@@ -34,7 +68,7 @@ public class EmployerController {
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -1,7 +1,84 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("skills")
 public class SkillController {
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("skills",skillRepository.findAll());
+        return "skills/index";
+    }
+
+
+    @GetMapping("view")
+    public String displayAllSkills(@RequestParam(required=false) Integer skillId, Model model){
+        if(skillId == null){
+            model.addAttribute("title", "All Skills");
+            model.addAttribute("skills", skillRepository.findAll());
+        } else {
+            Optional<Skill> result = skillRepository.findById(skillId);
+            if(result.isEmpty()){
+                model.addAttribute("title","Invalid Skill Id: " + skillId);
+            } else {
+                Skill skill = result.get();
+                model.addAttribute("title", "Skill with ID" + skillId);
+                model.addAttribute("skill",skill.getName());
+            }
+        }
+        return "skills/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if(!errors.hasErrors()) {
+
+            if (!skillRepository.toString().contains(newSkill.getName())) {
+                Skill skill = skillRepository.save(newSkill);
+                model.addAttribute("skill", skill);
+            }
+            return "redirect:";
+        }
+
+        return "redirect:";
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+        Optional optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skill = (Skill) optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+
 }
