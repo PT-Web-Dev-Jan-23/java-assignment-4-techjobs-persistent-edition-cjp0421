a772b707bbc4e368c8a4faa795ae502ba6406c26
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-06-03 13:43:28 -0500

    45/49 total tests pass

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     @Size(message = "Must be between 1 and 255 characters.",max = 255,min=1)
     private String description;
@@ -18,6 +24,14 @@ public class Skill extends AbstractEntity {
     }
 
     public Skill(){}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public String getDescription() {
         return description;
